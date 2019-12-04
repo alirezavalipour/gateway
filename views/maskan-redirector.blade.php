b792198a482caf4f600349bf758efe8d1a4a9998
alireza <alirezavalipour93@gmail.com>
2019-12-04 20:50:44 +0330

    addding maskan

---
+++ views/maskan-redirector.blade.php
@@ -0,0 +1,6 @@
+<form id="redirect_to_Maskan" method="POST" action="{{ $url }}" style="display:none !important;">
+    <input type="submit" value="Pay"/>
+</form>
+<script language="JavaScript" type="text/javascript">
+    document.getElementById("redirect_to_Maskan").submit();
+</script>
